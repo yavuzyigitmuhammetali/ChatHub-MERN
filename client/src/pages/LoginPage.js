ea0f465a4136266a52b8c96d9d8a6f24bdaa4ba8
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-12-14 15:44:41 +0300

    I enchanted login screen

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,9 +5,7 @@ import LoginForm from '../components/LoginForm';
 const LoginPage = () => {
   return (
     <Container component="main">
-      <Paper e sx={{ padding: 4, mt: 8 }}>
         <LoginForm />
-      </Paper>
     </Container>
   );
 };
